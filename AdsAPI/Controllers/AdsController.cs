641307f341cbc3ed2a97ffdaf3b63273804d844f
Nant1ng <andressantana99@hotmail.se>
2024-05-30 23:35:02 +0200

    GetAll ads works

---
+++ AdsAPI/Controllers/AdsController.cs
@@ -0,0 +1,27 @@
+﻿using AdsAPI.Data;
+using AdsAPI.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdsAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Ad>>> GetAll()
+        {
+            var ads = await _context.Ads.ToListAsync();
+
+            return Ok(ads);
+        }
+    }
+}
